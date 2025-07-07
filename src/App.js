cd852383ed05f8b9406c50a8e68f7dd330e37a3a
DevStorm22 <kaushikboss0@gmail.com>
2025-07-07 19:54:33 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -2,24 +2,25 @@ import React from 'react';
 import './App.css';
 import Home from './components/Home';
 import About from './components/About';
-import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Bioit from './components/Bioit';
 import Newsletter from './components/Newsletter';
 import Career from './components/Career';
-
+import Team from './components/Team';
 function App() {
-  
+
   return (
     <div>
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/about" element={<About/>}/>
-        <Route path="/bioit" element={<Bioit/>}/>
-        <Route path="/newsletter" element={<Newsletter/>}/>
-        <Route path='/career' element={<Career/>}/>
-      </Routes>
-    </Router>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/bioit" element={<Bioit />} />
+          <Route path="/newsletter" element={<Newsletter />} />
+          <Route path='/career' element={<Career />} />
+          <Route path='/team' element={<Team />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
